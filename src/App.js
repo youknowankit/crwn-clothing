e2d954d351312091dc56548a2ac67f1fa694d4ae
youknowankit <thisisankityadav@gmail.com>
2025-09-05 17:19:56 +0530

    Add the custom button component and finalize project for firebase authentication addition

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Routes } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header-component/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./form-input.styles.scss";
+
+const FormInput = ({ handleChange, label, ...OtherProps }) => {
+  return (
+    <div className="group">
+      <input className="form-input" onChange={handleChange} {...OtherProps} />
+      {label ? (
+        <label
+          className={`${
+            OtherProps.value && OtherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      ) : null}
+    </div>
+  );
+};
+
+export default FormInput;
